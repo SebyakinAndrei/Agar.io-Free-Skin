8746591679b41d3cf36190e7d9699b44d043aa4b
SebyakinAndrei <sebyakin.2332.sergei@yandex.ru>
2015-06-15 23:23:41 +0400

    Update Free Skin.user.js

--- Free Skin.user.js
+++ Free Skin.user.js
@@ -9,5 +9,5 @@
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.src = "http://downloader.disk.yandex.ru/disk/bad6ea083b2add93b41a296f9afc20462acedf6e7e73bed1779aec5e2947fa01/557f13b4/mFrTacgUGlP8gwfJHeb04qXeyxUFJZ6ofBCO4Rpb_xvo_czIrwWsT6jUZ0RnlayPGKyep1KFC0YZsLcKp1InUw%3D%3D?uid=122601390&filename=addon.js&disposition=attachment&hash=&limit=0&content_type=application%2Fjavascript&fsize=2575&hid=971772615c43255c876404412114df5f&media_type=development&tknv=v2&etag=2e28e670c5d4997dda8c6d75c4487dba";
+script.src = "https://raw.githubusercontent.com/SebyakinAndrei/Agar.io-Free-Skin/master/addon.js";
 (document.body || document.head || document.documentElement).appendChild(script);
