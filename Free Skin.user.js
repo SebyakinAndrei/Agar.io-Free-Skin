e7bcf4cea8abc4bb3d22520256a642cbd9aa5d2d
SebyakinAndrei <sebyakin.2332.sergei@yandex.ru>
2015-07-30 22:38:13 +0400

    Update Free Skin.user.js

--- Free Skin.user.js
+++ Free Skin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Free Skin
 // @namespace	 agarmods
-// @version      1.0.2
+// @version      1.0.3
 // @description  Your skin in agar.io!!!
 // @author       Sebyakin Andrei
 // @match        http://agar.io/
